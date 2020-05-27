e12234a9e7ba7215d70c4f61527fc8c4a1b3b34e
Sean Bordelon <sean.j.bordelon@gmail.com>
2020-05-27 06:17:20 +0000

    Automatically backed up by Learn

---
+++ reverse_each_word.rb
@@ -0,0 +1,26 @@
+def reverse_each_word (sentence)
+  
+  array = []
+  back = []
+  
+  array = sentence.split(" ")
+  index = 0
+  array.each do |words|
+    back[index] = words.reverse()
+    index += 1
+  end
+  
+  return back.join(" ")
+  
+end
+
+def reverse_each_word (sentence2)
+  
+  array2 = []
+  array2.collect do |words|
+    words.reverse
+  end
+  
+  return array2.join(" ")
+  
+end
