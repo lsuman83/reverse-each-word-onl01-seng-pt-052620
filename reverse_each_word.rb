1bde18e9195370c16ac61fe28f1d7e678d8a9d2f
Sean Bordelon <sean.j.bordelon@gmail.com>
2020-05-27 06:23:54 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -19,7 +19,7 @@ def reverse_each_word (sentence2)
   array2 = []
   array2 = sentence2.split(" ")
   array2.collect do |words|
-    words.reverse
+    words.reverse()
   end
   
   return array2.join(" ")
